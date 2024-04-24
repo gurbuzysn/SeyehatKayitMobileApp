7a368b089f25db8c4807a60a58fe3550341bf2d0
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-04-24 22:44:23 +0300

    Login Page

--- SeyehatKayitMobileApp/SeyehatKayitMobileApp/MainPage.xaml.cs
+++ SeyehatKayitMobileApp/SeyehatKayitMobileApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SeyehatKayitMobileApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,20 @@ namespace SeyehatKayitMobileApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+
+            if (isEmailEmpty || isPasswordEmpty)
+                return;
+
+            Navigation.PushAsync(new HomePage());
+
+
         }
     }
 }
