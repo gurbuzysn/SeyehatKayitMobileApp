6fe46a230c080e7033fa3e053fb91490ef8f5dc3
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-04-25 10:22:58 +0300

    Post Create

--- SeyehatKayitMobileApp/SeyehatKayitMobileApp/Pages/NewTravelPage.xaml.cs
+++ SeyehatKayitMobileApp/SeyehatKayitMobileApp/Pages/NewTravelPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SeyehatKayitMobileApp.Models;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,27 @@ namespace SeyehatKayitMobileApp.Pages
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Description = descriptionEntry.Text
+            };
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+
+            conn.CreateTable<Post>();
+
+            int rows = conn.Insert(post);
+
+            if (rows > 0)
+                DisplayAlert("Başarılı", "Açıklama başarı ile eklendi", "Tamam");
+            else
+                DisplayAlert("Hata", "Açıklama eklenirken bir hata oluştu", "Tamam");
+            
+            
         }
     }
 }
